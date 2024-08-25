b5cc31b15f2a037614082ea4f8de52962e8f67ca
Maisa Gabriela Rodrigues <maisagabirodrigues@gmail.com>
2024-08-24 23:32:32 -0300

    feat: header

--- js/script.js
+++ js/script.js
@@ -17,3 +17,35 @@ dropdownMenu.addEventListener('mouseleave', () => {
     dropdown.classList.remove('show');
     dropdownMenu.classList.remove('show');
 });
+
+const title = document.querySelector('.title-header');
+const texts = ['Sua Visão,', 'Nosso Design,', 'Seu Sucesso!'];
+let index = 0;
+let textIndex = 0;
+let currentText = '';
+
+function type() {
+    if (index < texts[textIndex].length) {
+        currentText = texts[textIndex].slice(0, ++index);
+        title.textContent = currentText;
+        setTimeout(type, 100);
+    } else {
+        setTimeout(erase, 2000);
+    }
+}
+
+function erase() {
+    if (index > 0) {
+        currentText = texts[textIndex].slice(0, --index);
+        title.textContent = currentText;
+        setTimeout(erase, 100);
+    } else {
+        textIndex = (textIndex + 1) % texts.length;  // Alterna entre os textos
+        setTimeout(type, 200);
+    }
+}
+
+// Inicia a animação
+type();
+
+
